c8caefa6689a4a324dd99a77d146f118ead639a3
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-08-23 16:44:39 -0700

    adds signup views in main_app/views

--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView
+from django.urls import reverse
 from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
-
+# Home view
 class Home(View):
     def get(self, request):
         return HttpResponse("Mom n Spot Home Page")
 
 
+# Signup view
+class Signup(View):
+    def get(self, request):
+        form = UserCreationForm()
+        context = {"form": form}
+        return render(request, "registration/signup.html", context)
+        
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("mom_n_pops_list")
 
