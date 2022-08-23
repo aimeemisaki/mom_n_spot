4718b2de0ab8dae97188d958332e5bb6e046362d
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-08-23 16:35:39 -0700

    adds account/signup url path in main_app/urls.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,4 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.Home.as_view(), name="home"),
+    path('accounts/signup/', views.Signup.as_view(), name="signup"  )
 ]
+
+#/
+# accounts/signup/
+# momnpops/
+# momnpops/<int:momnpopsId>
+# accounts/<int:accountId>
+# momnpops/category/<int:category_id>
+# momnpops/neighborhood/<int:neighborhood_id>
