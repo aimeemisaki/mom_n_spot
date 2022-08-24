bc6005e59c869c5c21469401c4eb974bb19cca10
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-08-24 15:42:29 -0700

    comments out upload path

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
-    path('/upload', )
+    # path('/upload', )
     # path('accounts/signup/', views.Signup.as_view(), name="signup"),
     # path('momnpops/', views.MomNPopsList.as_view(), name="momnpops_list"),
 ]
