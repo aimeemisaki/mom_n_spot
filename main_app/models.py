8e11f3aa58d44b1564937d7fcdcbcc1e9799fcee
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-08-23 20:10:11 -0700

    builds url path for momnpop index in main_app/urls.py

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
+import time
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Post(models.Model):
+    restaurant_name = models.CharField(max_lenght=200)
