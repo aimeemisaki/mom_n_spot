2eff67f2c4bec817315f4840d9f8aeaf79da0394
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-08-24 15:40:24 -0700

    test

--- mom_n_spot/settings.py
+++ mom_n_spot/settings.py
@@ -130,8 +130,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# LOGIN_REDIRECT_URL = '/'
-# LOGOUT_REDIRECT_URL = '/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 import django_on_heroku
 django_on_heroku.settings(locals())
+
+
