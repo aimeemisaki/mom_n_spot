f83ac1694c42095481148b46a621bd15bca72835
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-08-25 14:21:02 -0700

    builds simple form for image upload in upload.html

--- main_app/views.py
+++ main_app/views.py
@@ -43,6 +43,20 @@ class PostList(TemplateView):
             context["header"] = "Your Mom n Spots"
         return context
 
+# uploading image to form
+def upload(request):
+    if request.method == 'POST':
+        uploaded_file = request.FILES('img')
+        # uploaded_file.name 
+        # uploaded_file.size
+    return render(request, 'upload.html')
+
+class PostCreate(CreateView):
+    model = Post
+    fields = ('shop_name', 'address', 'neighborhood', 'story', 'category', 'img')
+    success_url = 'post_list'
+    template_name = 'upload.html'
+
 # # Post detail view
 # class PostDetail(DetailView):
 #     model = Post
@@ -56,48 +70,48 @@ class PostList(TemplateView):
 
 
 # PostCreateForm
-class PostCreateForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        exclude = ('user', )
+# class PostCreateForm(forms.ModelForm):
+#     class Meta:
+#         model = Post
+#         exclude = ('user', )
     
-    def __init__(self, *args, **kwargs):
-        self.user = kwargs.pop('user')
-        super(PostCreateForm, self).__init__(*args, **kwargs)
+#     def __init__(self, *args, **kwargs):
+#         self.user = kwargs.pop('user')
+#         super(PostCreateForm, self).__init__(*args, **kwargs)
     
-    # To make sure that no mom n spot has the same address
-    def clean_address(self):
-        address = self.cleaned_data['address']
-        if Post.objects.filter(user=self.user, address=address).exists():
-            raise forms.ValidationError("This Mom n Spot already exists in your list.")
-        return Post.shop_name
+#     # To make sure that no mom n spot has the same address
+#     def clean_address(self):
+#         address = self.cleaned_data['address']
+#         if Post.objects.filter(user=self.user, address=address).exists():
+#             raise forms.ValidationError("This Mom n Spot already exists in your list.")
+#         return Post.shop_name
 
 
-# Creating Post view
-@method_decorator(login_required, name='dispatch')
-class PostCreate(CreateView):
-    template_name = "post_create.html"
-    form_class = PostCreateForm
-    # fields = ['shop_name', 'img', 'story', 'category', 'neighborhood', 'address']
+# # Creating Post view
+# @method_decorator(login_required, name='dispatch')
+# class PostCreate(CreateView):
+#     template_name = "post_create.html"
+#     form_class = PostCreateForm
+#     # fields = ['shop_name', 'img', 'story', 'category', 'neighborhood', 'address']
 
     
-    def form_valid(self, forms):
-        self.object = forms.save(commit=False)
-        self.object.user = self.request.user
-        # form.instance.user = self.request.user
-        self.object.save()
-        return HttpResponseRedirect(self.get_success_url())
+#     def form_valid(self, forms):
+#         self.object = forms.save(commit=False)
+#         self.object.user = self.request.user
+#         # form.instance.user = self.request.user
+#         self.object.save()
+#         return HttpResponseRedirect(self.get_success_url())
 
-    # Initial Data
-    def get_initial(self, *args, **kwargs):
-        initial = super(PostCreate, self).get_initial(**kwargs)
-        return initial
+#     # Initial Data
+#     def get_initial(self, *args, **kwargs):
+#         initial = super(PostCreate, self).get_initial(**kwargs)
+#         return initial
 
-    # User is supplied from view during form creation
-    def get_form_kwargs(self, *args, **kwargs):
-        kwargs = super(PostCreate, self).get_form_kwargs(*args, **kwargs)
-        kwargs['user'] = self.request.user
-        return kwargs
+#     # User is supplied from view during form creation
+#     def get_form_kwargs(self, *args, **kwargs):
+#         kwargs = super(PostCreate, self).get_form_kwargs(*args, **kwargs)
+#         kwargs['user'] = self.request.user
+#         return kwargs
 
         # return super(PostCreate, self).form_valid(form)
     # def get_succcess_url(self):
