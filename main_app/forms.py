72e64853d9d6475de3b9d6b54224f0bddc259d09
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-08-26 16:05:16 -0700

    adds choices to neighborhood and category model field types in Post Model

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,10 @@
 from django import forms
 from .models import Post
 
+
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('shop_name', 'img', 'story', 'category', 'neighborhood', 'address', 'user')
+        
