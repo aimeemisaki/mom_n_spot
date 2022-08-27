4c2b70314e323d44bfb8b78fb46c06f1744913ea
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-08-26 19:12:06 -0700

    builds storage_backends.py

--- mom_n_spot/settings.py
+++ mom_n_spot/settings.py
@@ -15,6 +15,7 @@ load_dotenv()
 import os
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -135,7 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 
-
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
