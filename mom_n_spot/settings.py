45b4320359043650e07043dcdf6a8bb9c84fab8d
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-08-27 12:11:00 -0700

    adds to AWS config in settings.py and installs boto3 and django-storages

--- mom_n_spot/settings.py
+++ mom_n_spot/settings.py
@@ -31,20 +31,27 @@ DEBUG = True if os.getenv('MODE') == 'dev' else False
 
 ALLOWED_HOSTS = []
 
+# AWS CONFIGURATION 
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_S3_REGION_NAME = 'us-west-1'
-AWS_STORAGE_BUCKET_NAME = 'momnspot-static'
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
+AWS_DEFAULT_ACL = None
 AWS_LOCATION = 'static'
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_ROOT = 'https://momnspot-static.s3.amazonaws.com/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'main_app/static'),
 ]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'mom_n_spot.storage_backends.MediaStorage'
 
 # Application definition
 
