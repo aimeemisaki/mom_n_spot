6b2309cad82719406be0123f49f8a75a80f23ffd
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-08-28 11:07:34 -0700

    installs tailwind.config.js and postcss.config.js

--- main_app/views.py
+++ main_app/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from django.core.files.storage import FileSystemStorage
+
 
 
 
