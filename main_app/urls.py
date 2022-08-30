71b8cbd9226b2f72ebf4ebdea8fd1c61e279e360
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-08-30 03:25:53 -0700

    deletes commented out url paths

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,19 +10,6 @@ urlpatterns = [
     path('myposts/<int:pk>/', views.PostDetail.as_view(), name="post_detail"),
     path('myposts/<int:pk>/delete', views.PostDelete.as_view(), name="post_delete"),
     path('tags/<int:pk>/posts/<int:post_pk>', views.TagPostAssoc.as_view(), name="tag_post_assoc"),
-    # path('posts/', views.PostListAll.as_view(), name="post_list_all")
-    # path('posts/<int:pk>/update', views.PostUpdate.as_view(), name="artist_update"),
     
 ]
 
-# embed for pdf 
-
-#/
-# accounts/signup/
-# momnpops/
-# momnpops/<int:momnpopsId>
-# accounts/<int:accountId>
-# momnpops/category/<int:category_id>
-# momnpops/neighborhood/<int:neighborhood_id>
-
-# path('posts/upload/img/', views.upload, name='upload'),
