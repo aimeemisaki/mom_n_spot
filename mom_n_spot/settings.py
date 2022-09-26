c58b4307db83496aaefcdf3736d7bd63a05ab305
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-09-27 00:14:49 +0900

    adds file compression via WhiteNoise in settings.py

--- mom_n_spot/settings.py
+++ mom_n_spot/settings.py
@@ -158,6 +158,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
