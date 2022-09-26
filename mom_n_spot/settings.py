f84fbb114f5188689be5528d0c55f3f215b643cd
Aimee Misaki <93743792+aimeemisaki@users.noreply.github.com>
2022-09-26 21:03:43 +0900

    adds django browser reload middleware in settings.py

--- mom_n_spot/settings.py
+++ mom_n_spot/settings.py
@@ -78,6 +78,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'mom_n_spot.urls'
